7aab6ab94384b3003a98e4e44257f9f31cae4810
sarajouberta <102615255+sarajouberta@users.noreply.github.com>
2023-02-23 15:51:26 +0100

    Add files via upload
    prueba de crear repositorio con visual studio y hacer guion reales xDD

---
+++ 2-Reales.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	//1. codificacion de números reales
+
+	/*float f;
+	f = -27.625;
+
+	cout << "F:" << f << endl;
+
+
+	//2. codificación de enteros
+
+	float f1 = 0.1;
+	float f2 = 0.3;
+
+	cout << "F1:" << setprecision(15) << f1 << endl;
+	cout << "F2:" << setprecision(15) << f2 << endl;
+
+	const float TOLERANCIA = 0.0000001;
+	if (fabs(f1 * 3.0 - f2) < TOLERANCIA)
+		cout << "Son iguales" << endl;
+	else
+		cout << "Son distintos" << endl;
+
+
+	float euros = 0;
+	const int UN_MILLON = 1000000;
+	for (int i = 0; i < 20 * UN_MILLON; i++)
+		euros = euros + 1;
+	cout << "euros = " << euros << endl;
+
+
+	float f3 = 19 * UN_MILLON;
+	cout << "f3: " << setprecision(15) << f3 << endl;
+	f3 = f3 + 1;
+	cout << "f3 + 1: " << setprecision(15) << f3 << endl;*/
+
+
+	//3. codificación de caracteres
+
+	char c1 = 'a';
+	char c2 = 'ñ';
+
+	cout << c1 << " " << c2 << endl;
+	
+}
